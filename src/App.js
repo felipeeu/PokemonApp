f2762b1bf10855824695a5710dcfc246f0ab72bf
Felipe Domingues <>
2020-08-04 16:15:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [payloadPokemon, setPayloadPokemon] = useState([]);
+  const [offset, setOffset]= useState(0)
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=10`)
+      .then(response =>
+        response ? setPayloadPokemon(response.data.results) : null
+      );
+  },[offset]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {payloadPokemon &&
+        payloadPokemon.map((pokemon, idx) => <div key={idx}>{pokemon.name}</div>)}
+        <button onClick= {()=> setOffset(offset+ 10)  }>previous</button>
+        <button onClick= {()=> setOffset(offset +10)} >next</button>
     </div>
   );
 }
